2f3787837330776ea671b2b993b9832ce4658ee6
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-07-02 22:21:30 +0200

    setup router so that the app functions with a basic text interface

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,24 @@
 require_relative 'router.rb'
+require_relative 'family.rb'
+familyA = Family.new(
+  name: 'Family A',
+  pay_periods: [
+    {start: 5, end: 11, rate: 15},
+    {start: 11, end: 4, rate: 20}
+    ]);
+familyB = Family.new(
+  name: 'Family B',
+  pay_periods: [
+    {start: 5, end: 11, rate: 15},
+    {start: 11, end: 4, rate: 20}
+    ]);
+familyC = Family.new(
+  name: 'Family C',
+  pay_periods: [
+    {start: 5, end: 11, rate: 15},
+    {start: 11, end: 4, rate: 20}
+    ]);
 
-router = Router.new
+router = Router.new(familyA: familyA, familyB: familyB, familyC: familyC)
 
 router.run
